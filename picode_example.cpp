b644a1d1417c94889cab83e6c026e3de5cebc01c
Jorge Rivera <latchdevel@users.noreply.github.com>
2021-12-13 23:04:17 +0100

    Add build info to picode_example

--- picode_example.cpp
+++ picode_example.cpp
@@ -11,13 +11,37 @@
 #include "src/PiCode.h"
 #include <stdio.h> 
 
+#define STRINGIFY2(X) #X
+#define STRINGIFY(X) STRINGIFY2(X)
+
+#ifndef BUILD_VERSION
+#define BUILD_VERSION no git info
+#endif
+
+#ifndef BUILD_COMPILER
+#define BUILD_COMPILER no compiler info
+#endif
+
+#ifdef DEBUG
+#define BUILD_TYPE "Debug"
+#else
+#define BUILD_TYPE "Release"
+#endif
+
 int main(){
+
+    printf("picode_example (%s)\n", STRINGIFY(BUILD_VERSION));
+    printf("Compiled at " __DATE__ " " __TIME__ " %s (%s)\n\n",STRINGIFY(BUILD_COMPILER), BUILD_TYPE );
+
+    int result = 0;
 
     /* Decode from pilight string */
 
     char* pilight_string = (char*) "c:011010100101011010100110101001100110010101100110101010101010101012;p:1400,600,6800@";
 
     char* decoded_string = PiCode.decodeString(pilight_string);
+
+    printf("String to decode: \"%s\"\n",pilight_string);
 
     if (decoded_string){
 
@@ -27,7 +51,8 @@ int main(){
         free(decoded_string);
     
     }else{
-        printf("Unable to decode string\n");
+        printf("ERROR: Unable to decode string.\n");
+        result--;
     }
 
     /* Encode to pilight string from json */
@@ -37,6 +62,8 @@ int main(){
 
     char* encoded_json_string = PiCode.encodeJson(json,repeats);
 
+    printf("\nJSON to encode: \"%s\"\n",json);
+
     if (encoded_json_string){
 
         printf("Encode successful:\n");
@@ -45,8 +72,9 @@ int main(){
         free(encoded_json_string);
     
     }else{
-        printf("Unable to encode\n");
+        printf("ERROR: Unable to encode JSON.\n");
+        result--;
     }
 
-    return 0;
+    return result;
 }
